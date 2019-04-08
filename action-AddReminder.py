61eb7aa856367e76e37ad322a4562c584b0d562b
plgmail <caguilary@alumnos.unex.es>
2019-04-08 22:42:56 +0200

    second commit

--- action-AddReminder.py
+++ action-AddReminder.py
@@ -10,34 +10,17 @@ import io
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(configparser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
-
-
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
-        return dict()
-
 def subscribe_intent_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+    fecha = message.slots.Fecha.first().value
+    med = message.slots.Medicamento.first().value
+    msg = "Okay, añadierdo recordatorio:tomar  " + med + " el "+fecha
+    hermes.publish_end_session(message.session_id, msg)
 
 
-def action_wrapper(hermes, intentMessage, conf):
-    {{#each action_code as |a|}}{{a}}
-    {{/each}}
 
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
     with Hermes(mqtt_options=mqtt_opts) as h:
-        h.subscribe_intent("Anadir", subscribe_intent_callback) \
-         .start()
-        h.subscribe_intent("Cambio", subscribe_intent_callback) \
+        h.subscribe_intent("caguilary:Anadir", subscribe_intent_callback) \
          .start()
