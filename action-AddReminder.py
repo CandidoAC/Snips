db60004a88032828a5c04278c87d16dcee515aa4
plgmail <caguilary@alumnos.unex.es>
2019-04-20 18:52:24 +0200

    Fix python version

--- action-AddReminder.py
+++ action-AddReminder.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import io
 import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
-from hermes_python.ontology import *
-import io
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
@@ -13,7 +12,7 @@ CONFIG_INI = "config.ini"
 def subscribe_intent_callback(hermes, intentMessage):
     fecha = intentMessage.slots.Fecha.first().value
     med = intentMessage.slots.Medicamento.first().value
-    msg = "Okay, añadiendo recordatorio:tomar  " + med + " el "+fecha
+    msg = "Okay, añadiendo recordatorio:tomar  " + med  + " el "+fecha
     hermes.publish_end_session(intentMessage.session_id, msg)
 
 
