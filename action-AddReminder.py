5b1beeb2e838c9faf420bf20114dcb6d58ba620a
plgmail <caguilary@alumnos.unex.es>
2019-04-23 12:44:23 +0200

    Fix ask-->dates

--- action-AddReminder.py
+++ action-AddReminder.py
@@ -29,11 +29,12 @@ def read_configuration_file(configuration_file):
     except (IOError, configparser.Error) as e:
         return dict()
 
-def subscribe_intent_callback(hermes, intentMessage):
+#Intent Añadir mdicamento
+def subscribe_Anadir_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+    action_wrapper_Anadir(hermes, intentMessage, conf)
 
-def action_wrapper(hermes, intentMessage,conf):
+def action_wrapper_Anadir(hermes, intentMessage,conf):
     fecha = intentMessage.slots.Fecha.first().value
     fecha=fecha [ :fecha.index('+')-1 ]
     date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
@@ -42,9 +43,23 @@ def action_wrapper(hermes, intentMessage,conf):
     msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
     hermes.publish_end_session(intentMessage.session_id, msg)
 
+#Intent cambiar usuario
+def subscribe_user_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper_user(hermes, intentMessage, conf)
+
+def action_wrapper_user(hermes, intentMessage,conf):
+    user = intentMessage.slots.user.first().value
+   
+    msg="Cambio de usuario a "+user
+    hermes.publish_end_session(intentMessage.session_id, msg)
+
+
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
     with Hermes(mqtt_options=mqtt_opts) as h:
-        h.subscribe_intent("caguilary:Anadir", subscribe_intent_callback) \
+        h.subscribe_intent("caguilary:Anadir", subscribe_Anadir_callback) \
+         .start()
+        h.subscribe_intent("caguilary:user", subscribe_user_callback) \
          .start()
