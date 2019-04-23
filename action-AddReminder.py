53c3f6ec47394264b99cc5cb85ffd9ec1605d67b
plgmail <caguilary@alumnos.unex.es>
2019-04-23 12:24:53 +0200

    Fix ask-->dates

--- action-AddReminder.py
+++ action-AddReminder.py
@@ -12,6 +12,14 @@ from datetime import datetime
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
+def minutes(i):
+	switcher={
+        0:"",
+        15:" y cuarto",
+        30:" y media",
+        45:" menos cuarto",
+		}
+	return switcher.get(i," y " + str(i))
 def read_configuration_file(configuration_file):
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
@@ -27,14 +35,13 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage,conf):
     fecha = intentMessage.slots.Fecha.first().value
-	print(fecha[:fecha.index('+')])
+    fecha=fecha [ :fecha.index('+')-1 ]
+    date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
+
     med = intentMessage.slots.Medicamento.first().value
-    msg = "Okay, añadiendo recordatorio:tomar  " + med  + " el "+fecha
+    msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar  " + med
     ##msg="Hello"
     hermes.publish_end_session(intentMessage.session_id, msg)
-
-
-
 
 
 if __name__ == "__main__":
