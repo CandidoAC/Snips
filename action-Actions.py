fb9942c3baeaabae035d37a5d57458127f031558
plgmail <caguilary@alumnos.unex.es>
2019-04-29 22:07:39 +0200

    Reminder

--- action-Actions.py
+++ action-Actions.py
@@ -40,7 +40,7 @@ def action_wrapper_Anadir(hermes, intentMessage,conf):
     fecha = intentMessage.slots.Fecha.first().value
     fecha=fecha [ :fecha.index('+')-1 ]
     date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
-    scheduler.add_job(recordatorio, 'date', run_date=fecha,id='job1',args=['med'], max_instances=10000)
+    scheduler.add_job(recordatorio, 'date', run_date=date,id='job1',args=['med'], max_instances=10000)
     med = intentMessage.slots.Medicamento.first().value
     msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
     hermes.publish_end_session(intentMessage.session_id, msg)
