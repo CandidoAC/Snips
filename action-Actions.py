9fbd4f197d3f1dd95d234827d760fb25c2cc7b66
plgmail <caguilary@alumnos.unex.es>
2019-04-29 12:51:04 +0200

    Add proactive event

--- action-Actions.py
+++ action-Actions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import time
+import io
 import configparser
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from datetime import timedelta
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
-import io
-from datetime import datetime
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
+scheduler = BackgroundScheduler()
+scheduler1 = BackgroundScheduler()
 
 def minutes(i):
     switcher={
@@ -41,6 +45,8 @@ def action_wrapper_Anadir(hermes, intentMessage,conf):
 
     med = intentMessage.slots.Medicamento.first().value
     msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
+    scheduler.add_job(recordatorio, 'date', run_date=fecha,id='job1',args=['med'])
+    scheduler.start()
     hermes.publish_end_session(intentMessage.session_id, msg)
 
 
@@ -53,14 +59,41 @@ def action_wrapper_user(hermes, intentMessage,conf):
     user = intentMessage.slots.user.first().value
    
     msg="Cambio de usuario a "+user
-    print(msg)
     hermes.publish_end_session(intentMessage.session_id, msg)
 
 
-if __name__ == "__main__":
+def recordatorio(med):
+    print('Evento detectado para : %s' % datetime.now())
+    #Para el recordatorio si no se ha dicho aceptar o algo así7
+    i=0
+    scheduler1.add_job(Acept, 'interval', minutes=5,id='job2',args=['med'])
+    scheduler1.start()
+    while (scheduler1.get_job('job2')!=None):
+        time.sleep(360)
+        print("Vez "+str(i+1))
+        i+=1
+        if(i==4):
+            scheduler1.remove_job('job2')
+
+    if scheduler1.get_job('job2')!=None:
+        print("Evento realizado")
+    else:
+        print("Evento ignorado")
+   
+
+def Acept(med):
+    print("Tomar "+med)
+    
+def subscribe_event_callback():
+    scheduler1.remove_job('job2')
+
+if __name__ == '__main__':
+    
     mqtt_opts = MqttOptions()
     with Hermes(mqtt_options=mqtt_opts) as h:
         h\
         .subscribe_intent("caguilary:Anadir", subscribe_Anadir_callback) \
         .subscribe_intent("caguilary:user", subscribe_user_callback) \
-        .start()
+        .subscribe_intent("caguilary:event", subscribe_event_callback) \
+        .start()"""
+    
