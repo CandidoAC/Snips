b470689f1a58fba008a5bd9abc97a67322747ce3
plgmail <caguilary@alumnos.unex.es>
2019-05-06 20:23:23 +0200

    file

--- action-Actions.py
+++ action-Actions.py
@@ -24,72 +24,8 @@ def minutes(i):
         45:" menos cuarto",
         }
     return switcher.get(i," y " + str(i))
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, configparser.Error) as e:
-        return dict()
-
-#Intent Añadir mdicamento
-def subscribe_Anadir_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper_Anadir(hermes, intentMessage, conf)
-
-def action_wrapper_Anadir(hermes, intentMessage,conf):
-    fecha = intentMessage.slots.Fecha.first().value
-    fecha=fecha [ :fecha.index('+')-1 ]
-    date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
-    med = intentMessage.slots.Medicamento.first().value
-    scheduler.add_job(recordatorio, 'date', run_date=date,id=fecha,args=['med'], max_instances=10000)
-    msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
-    hermes.publish_end_session(intentMessage.session_id, msg)
 
 
-#Intent cambiar usuario
-def subscribe_user_callback(hermes, intentMessage):
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper_user(hermes, intentMessage, conf)
-
-def action_wrapper_user(hermes, intentMessage,conf):
-    user = intentMessage.slots.user.first().value
-   
-    msg="Cambio de usuario a "+user
-    hermes.publish_end_session(intentMessage.session_id, msg)
-
-#Intent Acept
-def subscribe_event_callback(hermes, intentMessage):
-    action_wrapper_event(hermes, intentMessage, conf)
-
-def action_wrapper_event(hermes, intentMessage,conf):   
-    msg="Evento aceptado"
-    hermes.publish_end_session(intentMessage.session_id, msg)
-    scheduler1.remove_job('job2')
-
-
-def recordatorio(med):
-    print('Evento detectado para : %s' % datetime.now())
-    #Para el recordatorio si no se ha dicho aceptar o algo así7
-    i=0
-    scheduler1.add_job(Acept, 'interval', minutes=5,id='job2',args=['med'])
-    while (scheduler1.get_job('job2')!=None):
-        time.sleep(360)
-        print("Vez "+str(i+1))
-        i+=1
-        if(i==4):
-            scheduler1.remove_job('job2')
-
-    if scheduler1.get_job('job2')!=None:
-        print("Evento realizado")
-    else:
-        print("Evento ignorado")
-   
-
-def Acept(med):
-    print("Tomar "+med)
-    
 def t():
     global idFile
     idFile += 1
@@ -110,10 +46,85 @@ def Reminder(med):
     writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Recordatorio','Medicamento':med,'Fecha_Evento':'','Nombre_Usuario':'','Error_output':''})
     t()
 
+def AceptedReminder(med):
+    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Aceptado','Medicamento':med,'Fecha_Evento':'','Nombre_Usuario':'','Error_output':''})
+    t()
+
 def Error(mensaje):
     date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Error','Medicamento':'','Fecha_Evento':'','Nombre_Usuario':'','Error_output':mensaje})
     t()
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+    	Error('Error:Fichero no encontrado')
+        return dict()
+
+#Intent Añadir mdicamento
+def subscribe_Anadir_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper_Anadir(hermes, intentMessage, conf)
+
+def action_wrapper_Anadir(hermes, intentMessage,conf):
+    fecha = intentMessage.slots.Fecha.first().value
+    fecha=fecha [ :fecha.index('+')-1 ]
+    date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
+    med = intentMessage.slots.Medicamento.first().value
+    scheduler.add_job(recordatorio, 'date', run_date=date,id=fecha,args=['med'], max_instances=10000)
+    msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
+    add_Reminder(med,fecha)
+    hermes.publish_end_session(intentMessage.session_id, msg)
+
+
+#Intent cambiar usuario
+def subscribe_user_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper_user(hermes, intentMessage, conf)
+
+def action_wrapper_user(hermes, intentMessage,conf):
+    user = intentMessage.slots.user.first().value
+   
+    msg="Cambio de usuario a "+user
+    Change_User(user)
+    hermes.publish_end_session(intentMessage.session_id, msg)
+
+#Intent Acept
+def subscribe_event_callback(hermes, intentMessage):
+    action_wrapper_event(hermes, intentMessage, conf)
+
+def action_wrapper_event(hermes, intentMessage,conf):   
+    msg="Evento aceptado"
+    AceptedReminder(med)
+    hermes.publish_end_session(intentMessage.session_id, msg)
+    scheduler1.remove_job('job2')
+
+
+def recordatorio(med):
+    print('Evento detectado para : %s' % datetime.now())
+    #Para el recordatorio si no se ha dicho aceptar o algo así7
+    i=0
+    scheduler1.add_job(Acept, 'interval', minutes=5,id='job2',args=['med'])
+    while (scheduler1.get_job('job2')!=None):
+    	Reminder(med)
+        time.sleep(360)
+        print("Vez "+str(i+1))
+        i+=1
+        if(i==4):
+            scheduler1.remove_job('job2')
+
+    if scheduler1.get_job('job2')!=None:
+        print("Evento realizado")
+    else:
+        print("Evento ignorado")
+   
+
+def Acept(med):
+    print("Tomar "+med)
 
 
 if __name__ == '__main__':
