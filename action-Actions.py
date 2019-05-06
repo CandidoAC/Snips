e95029dafbe70047621b45668bd607ebf2f859cc
plgmail <caguilary@alumnos.unex.es>
2019-05-06 19:48:07 +0200

    volviendo a prueba funcional

--- action-Actions.py
+++ action-Actions.py
@@ -4,7 +4,6 @@ import time
 import io
 import configparser
 import pytz as tz
-import csv
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from datetime import timedelta
@@ -31,7 +30,6 @@ def read_configuration_file(configuration_file):
             conf_parser.readfp(f)
             return conf_parser.to_dict()
     except (IOError, configparser.Error) as e:
-        Error('Error:Fichero no encontrado')
         return dict()
 
 #Intent Añadir mdicamento
@@ -44,10 +42,9 @@ def action_wrapper_Anadir(hermes, intentMessage,conf):
     fecha=fecha [ :fecha.index('+')-1 ]
     date=datetime.strptime(fecha,"%Y-%m-%d %H:%M:%S")
     print(str(date.astimezone(tz.timezone('Europe/Madrid'))))
-    med = intentMessage.slots.Medicamento.first().value
     scheduler.add_job(recordatorio, 'date', run_date=date.astimezone(tz.timezone('Europe/Madrid')),id=fecha,args=['med'], max_instances=10000)
+    med = intentMessage.slots.Medicamento.first().value
     msg="Añadiendo recordatorio para el día  " + str(date.day) + " de " + str(date.month) + " del " + str(date.year) + " a las " + str(date.hour) + minutes(date.minute)+" tomar " + med
-    add_Reminder(med,fecha)
     hermes.publish_end_session(intentMessage.session_id, msg)
 
 
@@ -60,7 +57,6 @@ def action_wrapper_user(hermes, intentMessage,conf):
     user = intentMessage.slots.user.first().value
    
     msg="Cambio de usuario a "+user
-    Change_User(user)
     hermes.publish_end_session(intentMessage.session_id, msg)
 
 #Intent Acept
@@ -74,65 +70,37 @@ def action_wrapper_event(hermes, intentMessage,conf):
 
 
 def recordatorio(med):
-    date=datetime.now()
     print('Evento detectado para : %s' % datetime.now())
     #Para el recordatorio si no se ha dicho aceptar o algo así7
-    acept=False
     i=0
     scheduler1.add_job(Acept, 'interval', minutes=5,id='job2',args=['med'])
     while (scheduler1.get_job('job2')!=None):
-        Reminder(med)
         time.sleep(360)
         print("Vez "+str(i+1))
         i+=1
         if(i==4):
-            scheduler1.remove_job('job2')   
-            acept=True
-    f.close()
+            scheduler1.remove_job('job2')
+
+    if scheduler1.get_job('job2')!=None:
+        print("Evento realizado")
+    else:
+        print("Evento ignorado")
+   
 
 def Acept(med):
     print("Tomar "+med)
     
-def t():
-    global idFile
-    idFile += 1
-
-
-def add_Reminder(med,fecha):
-    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Añadir_Evento','Medicamento':med,'Fecha_Evento':fecha,'Nombre_Usuario':'','Error_output':''})
-    t()
-
-def Change_User(user):
-    date=datetime.now()
-    writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Cambio_Usuario','Medicamento':'','Fecha_Evento':'','Nombre_Usuario':user,'Error_output':''})
-    t()
-
-def Reminder(med):
-    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Recordatorio','Medicamento':med,'Fecha_Evento':'','Nombre_Usuario':'','Error_output':''})
-    t()
-
-def Error(mensaje):
-    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    writer.writerow({'id': idFile,  'Fecha':date,'Tipo':'Error','Medicamento':'','Fecha_Evento':'','Nombre_Usuario':'','Error_output':mensaje})
-    t()
 
 if __name__ == '__main__':
-    idFile=0
+    
     scheduler = BackgroundScheduler()
     scheduler.start()
     scheduler1 = BackgroundScheduler()
     scheduler1.start()
     mqtt_opts = MqttOptions()
-    with open('prueba.csv', 'a') as csvfile:
-            fieldnames = ['id', 'Fecha','Tipo','Medicamento','Fecha_Evento','Nombre_Usuario','Error_output']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            with Hermes(mqtt_options=mqtt_opts) as h:
-                h\
-                .subscribe_intent("caguilary:Anadir", subscribe_Anadir_callback) \
-                .subscribe_intent("caguilary:user", subscribe_user_callback) \
-                .subscribe_intent("caguilary:event", subscribe_event_callback) \
-                .start()
-    
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h\
+        .subscribe_intent("caguilary:Anadir", subscribe_Anadir_callback) \
+        .subscribe_intent("caguilary:user", subscribe_user_callback) \
+        .subscribe_intent("caguilary:event", subscribe_event_callback) \
+        .start()
