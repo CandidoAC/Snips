39c918dd78f26a2bacc99a5d1aff7354cabd072a
plgmail <caguilary@alumnos.unex.es>
2019-05-17 00:00:49 +0200

    Proactivity

--- action-Actions.py
+++ action-Actions.py
@@ -157,4 +157,5 @@ if __name__ == '__main__':
         .subscribe_intent("caguilary:Anadir", subscribe_Anadir_callback) \
         .subscribe_intent("caguilary:user", subscribe_user_callback) \
         .subscribe_intent("caguilary:event", subscribe_event_callback) \
+        .subscribe_intent_not_recognized(onIntentNotRecognized)\
         .start()
