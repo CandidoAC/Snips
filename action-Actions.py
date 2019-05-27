14aaf350d5acef6a11f5f9cdf7d11174e191b5cb
plgmail <caguilary@alumnos.unex.es>
2019-05-27 19:39:02 +0200

    Proactivity v2

--- action-Actions.py
+++ action-Actions.py
@@ -87,7 +87,7 @@ def action_wrapper_Anadir(hermes, intentMessage,conf):
     if((date - now).total_seconds()>0):
         t = Timer((date - now).total_seconds(), recordatorio,['default',med,fecha])
         t.start()"""
-    scheduler.add_job(recordatorio, 'date', run_date=date,id=fecha,args=[med,date,Snips.usr], max_instances=10000)
+    scheduler.add_job(recordatorio, 'date', run_date=date,id=fecha,args=[intentMessage.session_id,med,date,Snips.usr], max_instances=10000)
     hermes.publish_end_session(intentMessage.session_id, msg)
     #Intent cambiar usuario
 def subscribe_user_callback(hermes, intentMessage):
@@ -139,17 +139,17 @@ def say(intentMessage,text):
     mqttClient.publish_start_session_notification(intentMessage, text,None)
 
     
-def recordatorio(med,fecha,usr):
+def recordatorio(intentMessage,med,fecha,usr):
     print('Evento detectado para : %s' % datetime.now())
     e=Event(med,fecha,usr)
     say(intentMessage,'Evento:Toca '+med)
     Snips.addEvent(e)
-    scheduler1.add_job(recordatorioTomar, 'interval', seconds=5,id='job2',args=[e])
+    scheduler1.add_job(recordatorioTomar, 'interval', seconds=5,id='job2',args=[e,intentMessage])
     #t = Timer(5, recordatorioTomar,['default',Recordatorio])
     #t.start()
    
 
-def recordatorioTomar(e):
+def recordatorioTomar(e,intentMessage):
     print('¿Te has tomado ' +e.med+'?')
     """for x in range(len(Snips.Levent)): 
         fechaE=x.fecha
