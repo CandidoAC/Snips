092888b96ac35802d22027f484e7cde4997371eb
plgmail <caguilary@alumnos.unex.es>
2019-05-30 18:13:20 +0200

    Proactivity v2

--- action-Actions.py
+++ action-Actions.py
@@ -152,16 +152,16 @@ def recordatorio(intentMessage,med,fecha,usr):
 
 def recordatorioTomar(e,intentMessage):
     mqttClient.publish_start_session_action(site_id=intentMessage,
-            session_init_text="¿Te has tomado  "+e.med+"?",
-            session_init_intent_filter=["Aceptar","Negar"],
+            session_init_text="",
+            session_init_intent_filter=[],
             session_init_can_be_enqueued=False,
-            session_init_send_intent_not_recognized=False,
+            session_init_send_intent_not_recognized=True,
             custom_data=None)
     
     if(e.veces<6):
         print('¿Te has tomado ' +e.med+'?:Vez '+str(e.veces))
         e.IncrementarVeces()     
-        #mqttClient.publish_continue_session(intentMessage,"¿Te has tomado ' +e.med+'?", ["Aceptar","Negar"])  
+        mqttClient.publish_continue_session(intentMessage,"¿Te has tomado ' +e.med+'?", ["caguilary:Confirmar","caguilary:Negar"])  
     else:
         msg='Evento ignorado:tomar '+e.med
         scheduler1.remove_job('job2')
