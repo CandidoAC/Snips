f6d369b115dec418ad2038b4d90002b0ed6b7c46
plgmail <caguilary@alumnos.unex.es>
2019-05-31 12:47:42 +0200

    Proactivity v2

--- Events.py
+++ Events.py
@@ -16,8 +16,8 @@ class Snips(object):
     def Incrementar(self,event):
     	for x in range(len(Snips.Levent)):
 	    	print("Nombre:"+Recordatorio.med+",fecha:"+str(Recordatorio.fecha)+",veces:"+str(Recordatorio.veces))
-	        if (event==Snips.Levent[x]):
-	            Snips.Levent[x].IncrementarVeces()
+	    	if (event==Snips.Levent[x]):
+	        	Snips.Levent[x].IncrementarVeces()
 
     	enc=False
     	if(not (any(x for x in self.Levent if x.__eq__(event)))):
