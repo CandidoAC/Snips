f02297ae41c970584e45073b8662f2a0d008f1b5
plgmail <caguilary@alumnos.unex.es>
2019-09-07 09:46:56 +0200

    Change some messages

--- Database.py
+++ Database.py
@@ -63,6 +63,7 @@ class Database(object):
            user=s
            rep=bool(x[4])
            if(rep):
+               print(x[6])
                fecha=datetime.strptime(x[6],"%Y-%m-%d %H:%M:%S")
                when=str(x[8])+' '+x[7]
            else:
